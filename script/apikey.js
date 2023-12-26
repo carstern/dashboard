cca6e6b7515f2675d031c4649147ab5102b41e02
Caroline Edgren <caroline.edgren88@gmail.com>
2023-12-26 12:04:16 +0100

    Testing the API stuff

--- script/apikey.js
+++ script/apikey.js
@@ -1,3 +1,5 @@
 const API_KEY = "b476713d2c62a2855f8754e4cc62706c";
 
 export default API_KEY;
+
+//TEst
--- script/keytest.js
+++ script/keytest.js
@@ -1,3 +1,6 @@
 const API_KEY = 'b476713d2c62a2855f8754e4cc62706c';
 
 export default API_KEY;
+
+
+//TEST
